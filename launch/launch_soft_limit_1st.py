a39f7860c8f84c9f7d8b58259128e3ae668f653c
nakao-yuka <63439747+nakao-yuka@users.noreply.github.com>
2020-09-30 18:07:41 +0900

    Update launch_soft_limit_1st.py

--- launch/launch_soft_limit_1st.py
+++ launch/launch_soft_limit_1st.py
@@ -7,10 +7,10 @@ def generate_launch_description():
             package='ros2pkg_ogameasure',
             node_executable='checker',
             parameters=[
-                {'az_upper_1st_limit': ''},
-                {'az_lower_1st_limit': ''}
-                {'el_upper_1st_limit': ''}
-                {'el_lower_1st_limit': ''}
+                {'az_upper_1st_limit': '350'},
+                {'az_lower_1st_limit': '10'}
+                {'el_upper_1st_limit': '80'}
+                {'el_lower_1st_limit': '15'}
             ]
         )],
        )
