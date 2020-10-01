9a3572e6e775292621e2d0b3e5ea36a94f28736c
KondoHiroshi <43530692+KondoHiroshi@users.noreply.github.com>
2020-10-01 19:41:04 +0900

    Update launch_soft_limit_1st.py

--- launch/launch_soft_limit_1st.py
+++ launch/launch_soft_limit_1st.py
@@ -7,10 +7,10 @@ def generate_launch_description():
             package='necst2_telescope',
             node_executable='limit_1st',
             parameters=[
-                {'az_upper_1st_limit': 350},
-                {'az_lower_1st_limit': 10},
-                {'el_upper_1st_limit': 80},
-                {'el_lower_1st_limit': 15}
+                {'az_upper_1st_limit': 350.0},
+                {'az_lower_1st_limit': 10.0},
+                {'el_upper_1st_limit': 80.0},
+                {'el_lower_1st_limit': 15.0}
             ]
         )],
        )
