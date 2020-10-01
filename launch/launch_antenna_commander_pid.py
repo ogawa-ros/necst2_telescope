bd426293e14e214decf2bd9f3f7b52a9ba3956ee
Kaoru Nishikawa <68896036+KaoruNishikawa@users.noreply.github.com>
2020-10-01 20:03:20 +0900

    Update launch_antenna_commander_pid.py

--- launch/launch_antenna_commander_pid.py
+++ launch/launch_antenna_commander_pid.py
@@ -8,14 +8,14 @@ def generate_launch_description():
             node_executable='az_pid',
             parameters=[
                 {'p_coeff': 1.5,
-                'i_coeff': 0,
-                'd_coeff': 0,
-                'gear_ratio': 7200,
-                'pulseper360deg': 65536,
-                'pulse_a': 16384,
-                'pulse_b': 2500,
+                'i_coeff': 0.0,
+                'd_coeff': 0.0,
+                'gear_ratio': 7200.0,
+                'pulseper360deg': 65536.0,
+                'pulse_a': 16384.0,
+                'pulse_b': 2500.0,
                 'MOTOR_MAXSPEED': 32767.5,
-                'MOTOR_AZ_MAXSPEED': 320000},
+                'MOTOR_AZ_MAXSPEED': 320000.0},
             ]
         ), 
         Node(
@@ -23,14 +23,14 @@ def generate_launch_description():
             node_executable='el_pid',
             parameters=[
                 {'p_coeff': 1.5,
-                'i_coeff': 0,
-                'd_coeff': 0,
-                'gear_ratio': 7200,
-                'pulseper360deg': 65536,
-                'pulse_a': 16384,
-                'pulse_b': 2500,
+                'i_coeff': 0.0,
+                'd_coeff': 0.0,
+                'gear_ratio': 7200.0,
+                'pulseper360deg': 65536.0,
+                'pulse_a': 16384.0,
+                'pulse_b': 2500.0,
                 'MOTOR_MAXSPEED': 32767.5,
-                'MOTOR_AZ_MAXSPEED': 320000},
+                'MOTOR_AZ_MAXSPEED': 320000.0},
             ]
         )],
     )
