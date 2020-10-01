5b741ee467703e801bd0813c1d7fc0b00883cf6d
ogawahideo <ogawahideo@ogawahideonoMacBook-Air.local>
2020-10-01 15:35:33 +0900

    pci7415

--- necst2_telescope/pci7415.py
+++ necst2_telescope/pci7415.py
@@ -1,9 +1,10 @@
+node_name = 'pci7415_driver'
+
 import sys
 import time
 import queue
-import threading
 import rclpy
-import rospy
+from functools import partial
 
 import pyinterface
 print(pyinterface.__version__)
@@ -14,19 +15,66 @@ class pci7415_driver(object):
 
     def __init__(self, rsw_id, params):
         #（__init__における handler の組み込み場所）
+        self.node = rclpy.create_node(node_name)
+
+        self.node.declare_parameter('~node_name')
+        self.node.declare_parameter('~rsw_id')
+        self.node.declare_parameter('~use_axis')
+
+        self.name = self.node.get_parameter('~node_name').get_parameter_value().string_value
+        self.rsw_id = self.node.get_parameter('~rsw_id').get_parameter_value().string_value
+        self._use_axis = self.node.get_parameter('~use_axis').get_parameter_value().string_value
+
+        self.params = {}
+        for ax in self._use_axis:
+            params[ax] = {}
+            #params[ax]['mode'] = rospy.get_param('~{ax}_mode'.format(**locals()), default_mode)
+            #params[ax]['pulse_conf'] = [eval(rospy.get_param('~{ax}_pulse_conf'.format(**locals()), default_pulse_conf))] #evalとは？？？
+            self.node.declare_parameter('~{ax}_mode')
+            self.node.declare_parameter('~{ax}_pulse_conf')
+
+            self.params[ax]['mode'] = self.node.get_parameter('~{ax}_mode').get_parameter_value().string_value
+            self.params[ax]['pulse_conf'] = self.node.get_parameter('~{ax}_pulse_conf').get_parameter_value().string_value
+
+
+            self.mp = {}
+                # mp['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
+                # mp['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
+                # mp['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
+                # mp['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
+                # mp['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
+                # mp['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
+                # mp['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
+
+            self.node.declare_parameter('~{ax}_clock')
+            self.node.declare_parameter('~{ax}_acc_mode')
+            self.node.declare_parameter('~{ax}_low_speed')
+            self.node.declare_parameter('~{ax}_speed')
+            self.node.declare_parameter('~{ax}_acc')
+            self.node.declare_parameter('~{ax}_dec')
+            self.node.declare_parameter('~{ax}_step')
+
+
+            self.mp['clock'] = self.node.get_parameter('~{ax}_clock').get_parameter_value().string_value #mpにはselfはいらない？？　paramsに格納するから
+            self.mp['acc_mode'] = self.node.get_parameter('~{ax}_acc_mode').get_parameter_value().string_value
+            self.mp['low_speed'] = self.node.get_parameter('~{ax}_low_speed').get_parameter_value().string_value
+            self.mp['speed'] = self.node.get_parameter('~{ax}_speed').get_parameter_value().string_value
+            self.mp['acc'] = self.node.get_parameter('~{ax}_acc').get_parameter_value().string_value
+            self.mp['dec'] = self.node.get_parameter('~{ax}_dec').get_parameter_value().string_value
+            self.mp['step'] = self.node.get_parameter('~{ax}_step').get_parameter_value().string_value
+            self.params[ax]['motion'] = mp
+            continue
 
     #元driver
         self.func_queue = queue.Queue()
         self.pub = {}
-        self.use_axis = ''.join([ax for ax in params])
+        self.use_axis = ''.join([ax for ax in self.params])
 
-        self.params = params
-        self.mode = [params[ax]['mode'] for ax in self.use_axis]
-        self.motion = {ax: params[ax]['motion'] for ax in self.use_axis}
+        self.mode = [self.params[ax]['mode'] for ax in self.use_axis]
+        self.motion = {ax: self.params[ax]['motion'] for ax in self.use_axis}
 
 
     #元handler
-        self.rsw_id = rsw_id
         #use axis
         self.current_speed = {ax: 0 for ax in self.use_axis}
         self.current_step = {ax: 0 for ax in self.use_axis}
@@ -34,34 +82,32 @@ class pci7415_driver(object):
         self.last_direction = {ax: 0 for ax in self.use_axis}
         self.do_status = [0, 0, 0, 0]
 
-        self.move_mode = {ax: p['mode'] for ax, p in params.items()}
-        self.default_speed = {ax: p['motion']['speed'] for ax, p in params.items()}
-        self.low_speed = {ax: p['motion']['low_speed'] for ax, p in params.items()}
+        self.move_mode = {ax: p['mode'] for ax, p in self.params.items()}
+        self.default_speed = {ax: p['motion']['speed'] for ax, p in self.params.items()}
+        self.low_speed = {ax: p['motion']['low_speed'] for ax, p in self.params.items()}
 
-        self.mot = pyinterface.open(7415, rsw_id)
-        [self.mot.set_pulse_out(ax, 'method', params[ax]['pulse_conf']) for ax in self.use_axis]
+        self.mot = pyinterface.open(7415, self.rsw_id)
+        [self.mot.set_pulse_out(ax, 'method', self.params[ax]['pulse_conf']) for ax in self.use_axis]
         self.mot.set_motion(self.use_axis, self.mode, self.motion)
 
-        self.node = rclpy.create_node(node_name)
 
     #元handler 命令値を受け取る
 
 
+        base = '/pyinterface/pci7415/rsw{self.rsw_id}'.format(**locals())
 
         for ax in self.use_axis:
             b = '{base}/{ax}/'.format(**locals())
             # rospy.Subscriber(b+'step_cmd', std_msgs.msg.Int64, self.set_step, callback_args=ax)
-            self.node.create_subscription(std_msgs.msg.Int64, b+'step_cmd', self.set_step, callback_args=ax)
+            self.node.create_subscription(std_msgs.msg.Int64, b+'step_cmd', partial(self.set_step, ax))
 
             # rospy.Subscriber(b+'speed_cmd', std_msgs.msg.Float64, self.set_speed, callback_args=ax)
-            self.node.create_subscription(std_msgs.msg.Int64, b+'speed_cmd', self.set_speed, callback_args=ax)
+            self.node.create_subscription(std_msgs.msg.Int64, b+'speed_cmd', partial(self.set_speed, ax))
 
         for do_num in range(1,5):
-            self.node.create_subscription(std_msgs.msg.Int64, '{}/output_do{}_cmd'.format(base, do_num), self.set_do, callback_args=do_num)
+            self.node.create_subscription(std_msgs.msg.Int64, '{}/output_do{}_cmd'.format(base, do_num), partial(self.set_do, do_num))
             #rospy.Subscriber('{}/output_do{}_cmd'.format(base, do_num), std_msgs.msg.Int64, self.set_do, callback_args=do_num)
 
-        # この位置をどうしよう？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
-        time.sleep(0.5)
 
         # loop start 元driver
         # self.th = threading.Thread(target=self.loop)
@@ -198,77 +244,31 @@ class pci7415_driver(object):
 
 
 
-default_name = 'pci7415'
-default_rsw_id = '0'
-default_do_conf = "[0, 0, 0, 0]"
-default_use_axis = 'xyzu'
-default_pulse_conf = "{'PULSE': '0', 'OUT': '0', 'DIR': '0', 'WAIT': '0', 'DUTY': '0'}"
-default_mode = 'ptp'
-default_clock = 299
-default_acc_mode = 'acc_normal'
-default_low_speed = 200
-default_speed = 10000
-default_acc = 1000
-default_dec = 1000
-default_step = 0
+# default_name = 'pci7415'
+# default_rsw_id = '0'
+# default_do_conf = "[0, 0, 0, 0]"
+# default_use_axis = 'xyzu'
+# default_pulse_conf = "{'PULSE': '0', 'OUT': '0', 'DIR': '0', 'WAIT': '0', 'DUTY': '0'}"
+# default_mode = 'ptp'
+# default_clock = 299
+# default_acc_mode = 'acc_normal'
+# default_low_speed = 200
+# default_speed = 10000
+# default_acc = 1000
+# default_dec = 1000
+# default_step = 0
 
+def main(args=None):
+    rclpy.init(args = args)
+    pci7415=pci7415_driver()
+    rclpy.spin(pci7415.node)
+
+    pci7415.node.destory_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
+    main()
     #name = rospy.get_param('~node_name'.format(**locals()), default_name)
     #rospy.init_node(name)
     #rsw_id = rospy.get_param('~rsw_id', default_rsw_id)
     #use_axis = rospy.get_param('~use_axis', default_use_axis) # ex. 'xyzu', 'xy', or 'yu'
-
-    self.node = rclpy.create_node(node_name)
-
-    self.node.declare_parameter('~node_name')
-    self.node.declare_parameter('~rsw_id')
-    self.node.declare_parameter('~use_axis')
-
-    self.name = self.node.get_parameter('~node_name').get_parameter_value().string_value
-    self.rsw_id = self.node.get_parameter('~rsw_id').get_parameter_value().string_value
-    self.use_axis = self.node.get_parameter('~use_axis').get_parameter_value().string_value
-
-    params = {}
-    for ax in use_axis:
-        params[ax] = {}
-        #params[ax]['mode'] = rospy.get_param('~{ax}_mode'.format(**locals()), default_mode)
-        #params[ax]['pulse_conf'] = [eval(rospy.get_param('~{ax}_pulse_conf'.format(**locals()), default_pulse_conf))] #evalとは？？？
-        self.node.declare_parameter('~{ax}_mode')
-        self.node.declare_parameter('~{ax}_pulse_conf')
-
-        self.params[ax]['mode'] = self.node.get_parameter('~{ax}_mode').get_parameter_value().string_value
-        self.params[ax]['pulse_conf'] = self.node.get_parameter('~{ax}_pulse_conf').get_parameter_value().string_value
-
-
-        mp = {}
-            # mp['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
-            # mp['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
-            # mp['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
-            # mp['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
-            # mp['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
-            # mp['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
-            # mp['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
-
-        self.node.declare_parameter('~{ax}_clock')
-        self.node.declare_parameter('~{ax}_acc_mode')
-        self.node.declare_parameter('~{ax}_low_speed')
-        self.node.declare_parameter('~{ax}_speed')
-        self.node.declare_parameter('~{ax}_acc')
-        self.node.declare_parameter('~{ax}_dec')
-        self.node.declare_parameter('~{ax}_step')
-
-
-        self.mp['clock'] = self.node.get_parameter('~{ax}_clock').get_parameter_value().string_value #mpにはselfはいらない？？　paramsに格納するから
-        self.mp['acc_mode'] = self.node.get_parameter('~{ax}_acc_mode').get_parameter_value().string_value
-        self.mp['low_speed'] = self.node.get_parameter('~{ax}_low_speed').get_parameter_value().string_value
-        self.mp['speed'] = self.node.get_parameter('~{ax}_speed').get_parameter_value().string_value
-        self.mp['acc'] = self.node.get_parameter('~{ax}_acc').get_parameter_value().string_value
-        self.mp['dec'] = self.node.get_parameter('~{ax}_dec').get_parameter_value().string_value
-        self.mp['step'] = self.node.get_parameter('~{ax}_step').get_parameter_value().string_value
-        self.params[ax]['motion'] = mp
-        continue
-
-    driver = pci7415_driver(rsw_id, params)
-
-    rospy.spin()
