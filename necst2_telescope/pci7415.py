9780747bca24c233ed05fd01f8b06c8273b11788
ogawahideo <ogawahideo@ogawahideonoMacBook-Air.local>
2020-10-01 13:28:09 +0900

    pci7415.py

--- necst2_telescope/pci7415.py
+++ necst2_telescope/pci7415.py
@@ -2,7 +2,7 @@ import sys
 import time
 import queue
 import threading
-
+import rclpy
 import rospy
 
 import pyinterface
@@ -42,60 +42,68 @@ class pci7415_driver(object):
         [self.mot.set_pulse_out(ax, 'method', params[ax]['pulse_conf']) for ax in self.use_axis]
         self.mot.set_motion(self.use_axis, self.mode, self.motion)
 
+        self.node = rclpy.create_node(node_name)
+
     #元handler 命令値を受け取る
+
+
+
         for ax in self.use_axis:
             b = '{base}/{ax}/'.format(**locals())
-            rospy.Subscriber(b+'step_cmd', std_msgs.msg.Int64, self.set_step, callback_args=ax)
-            rospy.Subscriber(b+'speed_cmd', std_msgs.msg.Float64, self.set_speed, callback_args=ax)
+            # rospy.Subscriber(b+'step_cmd', std_msgs.msg.Int64, self.set_step, callback_args=ax)
+            self.node.create_subscription(std_msgs.msg.Int64, b+'step_cmd', self.set_step, callback_args=ax)
 
+            # rospy.Subscriber(b+'speed_cmd', std_msgs.msg.Float64, self.set_speed, callback_args=ax)
+            self.node.create_subscription(std_msgs.msg.Int64, b+'speed_cmd', self.set_speed, callback_args=ax)
 
         for do_num in range(1,5):
-            rospy.Subscriber('{}/output_do{}_cmd'.format(base, do_num), std_msgs.msg.Int64, self.set_do, callback_args=do_num)
+            self.node.create_subscription(std_msgs.msg.Int64, '{}/output_do{}_cmd'.format(base, do_num), self.set_do, callback_args=do_num)
+            #rospy.Subscriber('{}/output_do{}_cmd'.format(base, do_num), std_msgs.msg.Int64, self.set_do, callback_args=do_num)
 
         # この位置をどうしよう？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？？
         time.sleep(0.5)
 
         # loop start 元driver
-        self.th = threading.Thread(target=self.loop)
-        self.th.setDaemon(True)
-        self.th.start()
+        # self.th = threading.Thread(target=self.loop)
+        # self.th.setDaemon(True)
+        # self.th.start()
+        self.node.create_timer(1e-5,self.loop)
 
         return
 
 
 
+
     #　元driver
     def loop(self):
-        while not rospy.is_shutdown():
+        # while not rospy.is_shutdown():
             #t0 = time.time()
-            speed = self.mot.read_speed(self.use_axis)
-            step = self.mot.read_counter(self.use_axis, cnt_mode='counter')
-            moving = self.mot.driver.get_main_status(self.use_axis)
+        speed = self.mot.read_speed(self.use_axis)
+        step = self.mot.read_counter(self.use_axis, cnt_mode='counter')
+        moving = self.mot.driver.get_main_status(self.use_axis)
             #t1 = time.time()
          # speed, step はそのまま辞書に入れる　　for文はいらない
 
-            self.current_speed = {ax: _ for ax, _ in zip(self.use_axis, speed)}
-            self.current_step = {ax: _ for ax, _ in zip(self.use_axis, step)}
-            self.current_moving = {ax: _ for ax, _ in zip(self.use_axis, moving)}
+        self.current_speed = {ax: _ for ax, _ in zip(self.use_axis, speed)}
+        self.current_step = {ax: _ for ax, _ in zip(self.use_axis, step)}
+        self.current_moving = {ax: _ for ax, _ in zip(self.use_axis, moving)}
 
             # 所要時間を測る
             #t2 = time.time()
             #self.pub_dt1.publish(t1-t0)
             #self.pub_dt2.publish(t2-t1)
             #self.pub_qsize.publish(self.func_queue.qsize())
-            if not self.func_queue.empty():
-                f = self.func_queue.get()
-                f['func'](f['data'], f['axis'])
-            else:
-                pass
+        if not self.func_queue.empty():
+            f = self.func_queue.get()
+            f['func'](f['data'], f['axis'])
+        else:
+            pass
 
-            time.sleep(1e-5)
+        #time.sleep(1e-5)
             # 要検討
-            continue
 
-if　
     def set_speed(self, speed, ax):
-        if abs(speed.data) < self.low_speed[ax]:
+        if abs(speed) < self.low_speed[ax]:
             #pub stop
             self.func_queue.put({'func': self.stop, 'data': 1, 'axis': ax}) # ->req data は１のこと？？？？？???????
             while self.current_moving[ax] != 0:
@@ -134,6 +142,22 @@ if　
 
 
         return
+
+    def set_step(self, step, ax):
+        if self.move_mode[ax] == 'ptp':
+            if self.current_moving[ax] != 0:
+                #pub change_step
+                self.func_queue.put({'func': self.change_speed, 'data': step.data, 'axis': ax})
+
+            else:
+                speed_step = [abs(self.default_speed[ax]), step.data]
+                speed_step_array = std_msgs.msg.Float64MultiArray()
+                speed_step_array.data = speed_step
+                self.func_queue.put({'func': self.start, 'data': speed_step_array, 'axis': ax})
+                pass
+        else: pass
+        return
+
 　　#元handler
     def set_do(self, do, do_num):
         self.do_status[do_num-1] = do.data
@@ -190,28 +214,59 @@ default_step = 0
 
 
 if __name__ == '__main__':
-    name = rospy.get_param('~node_name'.format(**locals()), default_name)
-    rospy.init_node(name)
+    #name = rospy.get_param('~node_name'.format(**locals()), default_name)
+    #rospy.init_node(name)
+    #rsw_id = rospy.get_param('~rsw_id', default_rsw_id)
+    #use_axis = rospy.get_param('~use_axis', default_use_axis) # ex. 'xyzu', 'xy', or 'yu'
 
-    rsw_id = rospy.get_param('~rsw_id', default_rsw_id)
-    use_axis = rospy.get_param('~use_axis', default_use_axis) # ex. 'xyzu', 'xy', or 'yu'
+    self.node = rclpy.create_node(node_name)
+
+    self.node.declare_parameter('~node_name')
+    self.node.declare_parameter('~rsw_id')
+    self.node.declare_parameter('~use_axis')
+
+    self.name = self.node.get_parameter('~node_name').get_parameter_value().string_value
+    self.rsw_id = self.node.get_parameter('~rsw_id').get_parameter_value().string_value
+    self.use_axis = self.node.get_parameter('~use_axis').get_parameter_value().string_value
 
     params = {}
     for ax in use_axis:
         params[ax] = {}
-        params[ax]['mode'] = rospy.get_param('~{ax}_mode'.format(**locals()), default_mode)
-        #p['do_conf'] = eval(rospy.get_param('~do_conf', default_do_conf))
-        params[ax]['pulse_conf'] = [eval(rospy.get_param('~{ax}_pulse_conf'.format(**locals()), default_pulse_conf))]
+        #params[ax]['mode'] = rospy.get_param('~{ax}_mode'.format(**locals()), default_mode)
+        #params[ax]['pulse_conf'] = [eval(rospy.get_param('~{ax}_pulse_conf'.format(**locals()), default_pulse_conf))] #evalとは？？？
+        self.node.declare_parameter('~{ax}_mode')
+        self.node.declare_parameter('~{ax}_pulse_conf')
+
+        self.params[ax]['mode'] = self.node.get_parameter('~{ax}_mode').get_parameter_value().string_value
+        self.params[ax]['pulse_conf'] = self.node.get_parameter('~{ax}_pulse_conf').get_parameter_value().string_value
+
 
         mp = {}
-        mp['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
-        mp['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
-        mp['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
-        mp['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
-        mp['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
-        mp['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
-        mp['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
-        params[ax]['motion'] = mp
+            # mp['clock'] = rospy.get_param('~{ax}_clock'.format(**locals()), default_clock)
+            # mp['acc_mode'] = rospy.get_param('~{ax}_acc_mode'.format(**locals()), default_acc_mode)
+            # mp['low_speed'] = rospy.get_param('~{ax}_low_speed'.format(**locals()), default_low_speed)
+            # mp['speed'] = rospy.get_param('~{ax}_speed'.format(**locals()), default_speed)
+            # mp['acc'] = rospy.get_param('~{ax}_acc'.format(**locals()), default_acc)
+            # mp['dec'] = rospy.get_param('~{ax}_dec'.format(**locals()), default_dec)
+            # mp['step'] = rospy.get_param('~{ax}_step'.format(**locals()), default_step)
+
+        self.node.declare_parameter('~{ax}_clock')
+        self.node.declare_parameter('~{ax}_acc_mode')
+        self.node.declare_parameter('~{ax}_low_speed')
+        self.node.declare_parameter('~{ax}_speed')
+        self.node.declare_parameter('~{ax}_acc')
+        self.node.declare_parameter('~{ax}_dec')
+        self.node.declare_parameter('~{ax}_step')
+
+
+        self.mp['clock'] = self.node.get_parameter('~{ax}_clock').get_parameter_value().string_value #mpにはselfはいらない？？　paramsに格納するから
+        self.mp['acc_mode'] = self.node.get_parameter('~{ax}_acc_mode').get_parameter_value().string_value
+        self.mp['low_speed'] = self.node.get_parameter('~{ax}_low_speed').get_parameter_value().string_value
+        self.mp['speed'] = self.node.get_parameter('~{ax}_speed').get_parameter_value().string_value
+        self.mp['acc'] = self.node.get_parameter('~{ax}_acc').get_parameter_value().string_value
+        self.mp['dec'] = self.node.get_parameter('~{ax}_dec').get_parameter_value().string_value
+        self.mp['step'] = self.node.get_parameter('~{ax}_step').get_parameter_value().string_value
+        self.params[ax]['motion'] = mp
         continue
 
     driver = pci7415_driver(rsw_id, params)
