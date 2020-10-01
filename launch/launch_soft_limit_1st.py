e78fd2057fb19fe920fba976f98fa829b034185b
nakao-yuka <63439747+nakao-yuka@users.noreply.github.com>
2020-10-01 19:55:58 +0900

    Update launch_soft_limit_1st.py

--- launch/launch_soft_limit_1st.py
+++ launch/launch_soft_limit_1st.py
@@ -8,9 +8,9 @@ def generate_launch_description():
             node_executable='limit_1st',
             parameters=[
                 {'az_upper_1st_limit': 350.0,
-                　'az_lower_1st_limit': 10.0,
-                　'el_upper_1st_limit': 80.0,
-                　'el_lower_1st_limit': 15.0}
+                'az_lower_1st_limit': 10.0,
+                'el_upper_1st_limit': 80.0,
+                'el_lower_1st_limit': 15.0}
             ]
         )],
        )
