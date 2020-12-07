e517259d8406cedf446f375225b60c157cd53cbc
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2020-12-07 09:47:44 +0900

    add executor concepts

---
+++ executor/exec_antenna_commander.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from ..necst2_telescope.antenna_commander_pid import antenna_commander_pid
+from ..necst2_telescope.soft_limit_1st import checker
+from ..necst2_telescope.soft_limit_2nd import motor_locker
+
+import rclpy
+from rclpy.executors import SingleThreadedExecutor
+
+def main(args=None):
+    rclpy.init(args=args)
+    try:
+        nodes = {
+            "pid": antenna_commander_pid(),
+            "soft1st": checker(),
+            "soft2nd": motor_locker(),
+        }
+
+        executor = SingleThreadedExecutor()
+
+        [executor.add_node(node) for node in nodes.values()]
+
+        try:
+            executor.spin()
+        finally:
+            executor.shutdown()
+            [node.destroy_node() for node in nodes.values()]
+    finally:
+        rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
