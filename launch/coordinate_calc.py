49b5038d9a1f61e00b2d2aba6f2856e32d6e4651
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2021-02-08 12:15:09 +0900

    Resolve untracked names

--- launch/coordinate_calc.py
+++ launch/coordinate_calc.py
@@ -1,13 +1,14 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
+
 def generate_launch_description():
-    return LaunchDescription([
-        Node(
-            package='necst2_telescope',
-            node_executable='lcoordinate_calc',
-            parameters=[
-                {}
-            ]
-        )],
-       )
+    return LaunchDescription(
+        [
+            Node(
+                package="necst2_telescope",
+                node_executable="coordinate_calc",
+                parameters=[{}],
+            )
+        ],
+    )
--- setup.py
+++ setup.py
@@ -1,30 +1,37 @@
 from setuptools import setup
 
-package_name = 'necst2_telescope'
+package_name = "necst2_telescope"
+
+scripts = [
+    "soft_limit_1st",
+    "soft_limit_2nd",
+    "antenna_commander_pid",
+    "coordinate_calc",
+    "optical_pointing",
+    "tpro01",
+]  # reconsider when scripts' directory separated or its hierarchical structure changed.
+executors = [
+    "exec_antenna_commander",
+]
+
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version="0.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
-    maintainer='telescopio',
-    maintainer_email='s_ta.matsumoto@p.s.osakafu-u.ac.jp',
-    description='TODO: Package description',
-    license='TODO: License declaration',
-    tests_require=['pytest'],
+    maintainer="telescopio",
+    maintainer_email="s_ta.matsumoto@p.s.osakafu-u.ac.jp",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts':[
-            'limit_1st='+package_name+'.soft_limit_1st:main',
-            'limit_2nd='+package_name+'.soft_limit_2nd:main',
-            'az_pid='+package_name+'.antenna_az_commander_pid:main',
-            'el_pid='+package_name+'.antenna_el_commander_pid:main',
-            'tpro01='+package_name+'.tpro01:main'
-            ],
+        "console_scripts": [f"{name}={package_name}.{name}:main" for name in scripts]
+        + [f"{name}={package_name}.{name}:main" for name in executors],
     },
 )
